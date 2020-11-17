acff0aef0122a055e2e1ad83b22295222e44d877
RO03M <romera.gv.s@gmail.com>
2020-11-17 11:43:38 -0300

    AimRobot Added
    AimRobot entity and behaviour added

---
+++ Assets/Scripts/Enemies/AimRobot/AimRobotTurret.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimRobotTurret : MonoBehaviour {
+
+    public GameObject aimRobotBullet;
+    public float bulletSpeed = 5;
+    public float cloneDestroyTime = 5;
+    public float fireDelay;
+    public float minDistance;
+
+    protected bool canFire = true;
+    protected bool isPlayable;
+
+    private AimRobotBehaviour aimRobotBehaviour;
+    private float zAngle;
+
+    private void Start() {
+        aimRobotBehaviour = gameObject.GetComponentInParent<AimRobotBehaviour>();
+        minDistance = aimRobotBehaviour.minDistance;
+        fireDelay = aimRobotBehaviour.fireDelay;
+    }
+
+    private void Update() {
+        this.isPlayable = aimRobotBehaviour.isPlayable;
+        if (isPlayable) {
+            LookAt();
+            if (Input.GetMouseButtonDown(0) && canFire) Fire();
+        } else DefaultBehaviour();
+    }
+
+    private void DefaultBehaviour() {
+        Vector3 playerPosition = GameObject.Find("Player").transform.position;
+        float currentDistance = Vector3.Distance(transform.position, playerPosition);
+        if (currentDistance <= minDistance) {
+            LookAt(playerPosition);
+            Fire();
+        }
+    }
+
+    private void LookAt() {
+        Vector3 thisPosition = this.transform.position;
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 positionInRadius = Vector3.zero;
+        positionInRadius.x = targetPosition.x - thisPosition.x;
+        positionInRadius.y = targetPosition.y - thisPosition.y;
+        zAngle = Mathf.Atan2(positionInRadius.x, positionInRadius.y) * -Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, zAngle));
+    }
+
+    private void LookAt(Vector3 lookTarget) {
+        Vector3 thisPosition = this.transform.position;
+        Vector3 targetPosition = lookTarget;
+        Vector3 positionInRadius = Vector3.zero;
+        positionInRadius.x = targetPosition.x - thisPosition.x;
+        positionInRadius.y = targetPosition.y - thisPosition.y;
+        zAngle = Mathf.Atan2(positionInRadius.x, positionInRadius.y) * -Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, zAngle));
+    }
+
+    private void Fire() {
+        if (!canFire) return;
+        Quaternion quaternionAngle = Quaternion.Euler(new Vector3(0, 0, zAngle));
+        float angle = quaternionAngle.eulerAngles.z;
+        angle *= Mathf.Deg2Rad;
+        float cos = Mathf.Cos(angle);
+        float sin = Mathf.Sin(angle);
+        GameObject bulletClone = Instantiate(aimRobotBullet, transform.position, quaternionAngle);
+        Rigidbody2D cloneRb = bulletClone.GetComponent<Rigidbody2D>();
+        cloneRb.velocity = new Vector2(cos * bulletSpeed, sin * bulletSpeed);
+        StartCoroutine(FireDelay());
+        Destroy(cloneRb, cloneDestroyTime);
+    }
+
+    private IEnumerator FireDelay() {
+        canFire = false;
+        yield return new WaitForSeconds(fireDelay);
+        canFire = true;
+    }
+
+}
---
+++ Assets/Scripts/Player/Interactions/AimRobot/DHCPower.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DHCPower : MonoBehaviour {
+
+    public float radius = 10;
+    public GameObject rippleEffect;
+    public float rippleTime = 2;
+    [Tooltip("Amount of times that the ripple effect will grow")]
+    public float rippleSmoothness = 100;
+
+    private int aimRobotCount = 0;
+    private bool canCallEffect = false;
+    private int callEffectCount = 0;
+    private float effectTime;
+    private int maxAimRobotDetection = 3;
+
+    private void Start() {
+        effectTime = rippleTime / rippleSmoothness;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.L)) {
+            AimRobotDetection();
+            canCallEffect = true;
+        }
+
+        if (canCallEffect && callEffectCount == 0) {
+            StartCoroutine(RippleIncrease());
+            callEffectCount++;
+        }
+    }
+
+    private void AimRobotDetection() {
+        Collider2D[] arrayObjects = Physics2D.OverlapCircleAll(this.transform.position, radius);
+        List<GameObject> aimRobots = GetNearAimRobots(arrayObjects);
+        for (int i = 0; i < aimRobots.Count; i++) {
+            aimRobots[i].GetComponent<AimRobotBehaviour>().isPlayable = true;
+        }
+    }
+
+    private List<GameObject> GetNearAimRobots(Collider2D[] arrayObjects) {
+        List<Collider2D> aimRobotArray = new List<Collider2D>();
+        List<GameObject> nearestAimRobots = new List<GameObject>();
+        for (int i = 0; i < arrayObjects.Length; i++) {
+            if (arrayObjects[i].gameObject.tag == "AimRobot") {
+                aimRobotArray.Add(arrayObjects[i]);
+            }
+        }
+        for (int i = 0; i < aimRobotArray.Count; i++) {
+            if (nearestAimRobots.Count < maxAimRobotDetection)
+                nearestAimRobots.Add(aimRobotArray[i].gameObject);
+            else {
+                for (int j = 0; j < nearestAimRobots.Count; j++) {
+                    float currentAimRobotDistance = Vector3.Distance(transform.position, aimRobotArray[i].gameObject.transform.position);
+                    float insiderArrayRobotDistance = Vector3.Distance(transform.position, nearestAimRobots[j].transform.position);
+                    if (currentAimRobotDistance < insiderArrayRobotDistance) nearestAimRobots[j] = aimRobotArray[i].gameObject;
+                }
+            }
+        }
+        return nearestAimRobots;
+    }
+
+    private IEnumerator RippleIncrease() {
+        float scaleIncreaseFactor = (radius * 2) / rippleSmoothness;
+        if (canCallEffect) rippleEffect.transform.localScale += new Vector3(scaleIncreaseFactor, scaleIncreaseFactor, 0);
+        float rippleEffectX = rippleEffect.transform.localScale.x;
+        float rippleEffectY = rippleEffect.transform.localScale.y;
+        if (rippleEffectX >= radius * 2 && rippleEffectY >= radius * 2) {
+            canCallEffect = false;
+            callEffectCount = 0;
+            rippleEffect.transform.localScale = Vector3.zero;
+            yield break;
+        }
+        yield return new WaitForSeconds(effectTime);
+        StartCoroutine(RippleIncrease());
+    }
+
+}
